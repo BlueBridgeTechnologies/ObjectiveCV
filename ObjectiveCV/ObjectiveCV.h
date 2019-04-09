644904f220fdf810268bfa60d42a4c5187226de4
Jack Costigan <jackcostigan@bluebridgetech.com>
2019-04-09 12:22:24 +0100

    Initial Commit

---
+++ ObjectiveCV/ObjectiveCV.h
@@ -0,0 +1,19 @@
+//
+//  ObjectiveCV.h
+//  ObjectiveCV
+//
+//  Created by admin on 09/04/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjectiveCV.
+FOUNDATION_EXPORT double ObjectiveCVVersionNumber;
+
+//! Project version string for ObjectiveCV.
+FOUNDATION_EXPORT const unsigned char ObjectiveCVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveCV/PublicHeader.h>
+
+
